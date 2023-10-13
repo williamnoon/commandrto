9fa8ff2b3da65c27e296f4f75ead253d125b1465
williamdeval@gmail.com <williamdeval@gmail.com>
2023-10-13 03:01:29 +0000

    Created ContactsComponent and MainForm; Edited settings and ServerModule1; Deleted Frame.RowTemplate1, Sales, Frame and Reports

---
+++ client_code/ContactsComponent/__init__.py
@@ -0,0 +1,28 @@
+from ._anvil_designer import ContactsComponentTemplate
+from anvil import *
+import anvil.server
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+# Any code you write here will run before the form opens.
+from anvil import FormTemplate, DataGrid, Button
+
+class ContactsComponent(FormTemplate):
+    def __init__(self, **properties):
+        self.init_components(**properties)
+        
+        self.title_label = Label(text="Contacts List")
+        self.add_component(self.title_label)
+        
+        self.contacts_datagrid = DataGrid()
+        self.add_component(self.contacts_datagrid)
+
+        self.refresh_button = Button(text="Refresh")
+        self.refresh_button.set_event_handler('click', self.refresh_data)
+        self.add_component(self.refresh_button)
+        
+    def refresh_data(self, **event_args):
+        contacts = anvil.server.call('get_all_contacts')
+        self.contacts_datagrid.items = contacts
---
+++ client_code/MainForm/__init__.py
@@ -0,0 +1,39 @@
+
+from anvil import *
+
+from .ContactsComponent import ContactsComponent
+
+class MainForm(FormTemplate):
+    def __init__(self, **properties):
+        self.init_components(**properties)
+        users = anvil.users.login_with_form()
+
+        self.sidebar = ColumnPanel()
+        self.add_component(self.sidebar)
+        
+        self.contacts_link = Link(text="Contacts")
+        self.contacts_link.set_event_handler('click', self.show_contacts)
+        self.sidebar.add_component(self.contacts_link)
+        
+        self.content_space = ColumnPanel()
+        self.add_component(self.content_space)
+
+    def show_contacts(self, **event_args):
+        self.content_space.clear()
+        contacts_component = ContactsComponent()
+        self.content_space.add_component(contacts_component)
+
+  
+  #If using the Users service, uncomment this code to log out the user:
+    def signout_link_click(self, **event_args):
+  #   """This method is called when the link is clicked"""
+     anvil.users.logout()
+     open_form('Logout')
+
+
+
+
+
+
+
+
